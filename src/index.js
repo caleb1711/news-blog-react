49d675abe190b5bfe6021a53dc30ef998d42f466
caleb1711 <Calkra15@outlook.com>
2024-02-14 03:36:20 +0500

    Add the new design changes in footer

--- src/index.js
+++ src/index.js
@@ -1,27 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./components/style/footer.css"
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Header from "./components/header";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
     <footer className="footer">
-        <div className="footerText">
+        {/* <div className="footerText">
             <h1>Your Blog Journey Starts Here</h1>
             <h3>Click here to get registered</h3>
             <a href="/signup" className="signup-link"><h2 className="text-danger">SIGN UP</h2></a>
-        </div>
+        </div> */}
         <div className="black-section">
             <div className="row">
                 <div className="col-lg-6 col-md-3 col-sm-4 logo-link-col">
-                    <a href="/home" className="logo-link">
+                    <a href="/" className="logo-link">
                         <div className="main_header_text">
-                        <a href="/home" style={{ textDecoration: 'none' }}>
+                        <a href="/" style={{ textDecoration: 'none' }}>
                           <h4>
                               Ne<span style={{ color: '#ea2d00' }}>w</span>s
                           </h4>
@@ -34,10 +32,10 @@ root.render(
                 <div className="col-lg-3 col-md-2 col-sm-4 col-12">
                     <ul className="list-unstyled">
                         <li>
-                            <a href="/home" className="nav-link">About</a>
+                            <a href="#" className="nav-link1">About</a>
                         </li>
                         <li>
-                            <a href="/home" className="nav-link">Contact Us</a>
+                            <a href="#" className="nav-link1">Contact Us</a>
                         </li>
                         
                     </ul>
@@ -46,13 +44,13 @@ root.render(
                 <div className="col-lg-3 col-md-2 col-sm-4 col-12">
                     <ul className="list-unstyled">
                         <li>
-                            <a href="{% url 'privacy' %}" className="nav-link">Privacy Policy</a>
+                            <a href="#" className="nav-link1">Privacy Policy</a>
                         </li>
                         <li>
-                            <a href="{% url 'terms' %}" className="nav-link">Terms & Conditions</a>
+                            <a href="#" className="nav-link1">Terms & Conditions</a>
                         </li>
                         <li>
-                            <a href="{% url 'disclaimer' %}" className="nav-link">Disclaimer</a>
+                            <a href="#" className="nav-link1">Disclaimer</a>
                         </li>
                     </ul>
                 </div>
