33171b6f3c0aa1293265aa46897f6cd864e12dea
caleb1711 <calkra15@outlook.com>
2024-02-13 21:49:44 +0000

    add new footer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./components/style/footer.css"
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -10,9 +11,57 @@ root.render(
   <React.StrictMode>
     <App />
     <footer className="footer">
-      <div className="footer-content">
-        <p>&copy; 2023 News Blog. All rights reserved.</p>
-      </div>
+        <div className="footerText">
+            <h1>Your Blog Journey Starts Here</h1>
+            <h3>Click here to get registered</h3>
+            <a href="/signup" className="signup-link"><h2 className="text-danger">SIGN UP</h2></a>
+        </div>
+        <div className="black-section">
+            <div className="row">
+                <div className="col-lg-6 col-md-3 col-sm-4 logo-link-col">
+                    <a href="/home" className="logo-link">
+                        <div className="main_header_text">
+                        <a href="/home" style={{ textDecoration: 'none' }}>
+                          <h4>
+                              Ne<span style={{ color: '#ea2d00' }}>w</span>s
+                          </h4>
+                          <p>Blog</p>
+                        </a>
+                        </div>
+                    </a>
+                </div>
+                
+                <div className="col-lg-3 col-md-2 col-sm-4 col-12">
+                    <ul className="list-unstyled">
+                        <li>
+                            <a href="/home" className="nav-link">About</a>
+                        </li>
+                        <li>
+                            <a href="/home" className="nav-link">Contact Us</a>
+                        </li>
+                        
+                    </ul>
+                </div>
+                
+                <div className="col-lg-3 col-md-2 col-sm-4 col-12">
+                    <ul className="list-unstyled">
+                        <li>
+                            <a href="{% url 'privacy' %}" className="nav-link">Privacy Policy</a>
+                        </li>
+                        <li>
+                            <a href="{% url 'terms' %}" className="nav-link">Terms & Conditions</a>
+                        </li>
+                        <li>
+                            <a href="{% url 'disclaimer' %}" className="nav-link">Disclaimer</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        
+       <div className="copy_rightDiv">
+        <p className="copy_right">© 2024 NewsBlog. All rights reserved</p>
+       </div>
     </footer>
   </React.StrictMode>
 );
